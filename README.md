efcaa32ced07ad501e1b8462c3fec40b9db123e8
Qi <wqstu1@gmail.com>
2024-07-16 21:08:16 -0700

    Commit

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-Execution:
+# 1. Execution
 
 ```
 python ./main.py
 ```
 
+# 2. Concepts
+
+## 2.1 Initial Cost
+
+Initial costs are the costs you incur when you go to the closing for the home you are purchasing. This includes the down payment and other fees.
+
+## 2.2 Reccuring Costs
+
+Recurring costs are expenses you will have to pay monthly or yearly in owning your home. These include mortgage payments; condo fees (or other community living fees); maintenance and renovation costs; property taxes; and homeowner’s insurance. A few items are tax deductible, up to a point: property taxes; the interest part of the mortgage payment; and, in some cases, a portion of the common charges.
+
+The resulting tax savings are accounted for in the buying total. If your house-related deductions are similar to or smaller than the standard deduction, you’ll get little or no relative tax savings from buying. If your house-related deductions are large enough to make itemizing worthwhile, we only count as savings the amount above the standard deduction.
+
+## 2.3 Opportunity Costs
+
+Opportunity costs are calculated for the initial purchase costs and for the recurring costs. That will give you an idea of how much you could have made if you had invested your money instead of buying your home.
+
+## 2.4 Net Proceeds
+
+Net proceeeds is the amount of money you receive from the sale of your home minus the closing costs, which includes the broker’s commission and other fees, the remaining principal balance that you pay to your mortgage bank and any tax you have to pay on profit that exceeds your capital gains exclusion. If your total is negative, it means you have done very well: You made enough of a profit that it covered not only the cost of your home, but also all of your recurring expenses.
--- main.py
+++ main.py
@@ -18,39 +18,6 @@ home_growth = 0.094 # Use data point 1
 # 1962 - 2023 Not adjusted for inflation
 investment_return = 0.102
 
-"""
-# Initial Cost:
-# Initial costs are the costs you incur when you go to the closing for the home
-# you are purchasing. This includes the down payment and other fees.
-
-# Reccuring Costs:
-# "Recurring costs are expenses you will have to pay monthly or yearly in
-# owning your home. These include mortgage payments; condo fees (or other
-# community living fees); maintenance and renovation costs; property taxes; and
-# homeowner’s insurance. A few items are tax deductible, up to a point: property
-# taxes; the interest part of the mortgage payment; and, in some cases, a
-# portion of the common charges.
-# The resulting tax savings are accounted for in the buying total. If your
-# house-related deductions are similar to or smaller than the standard deduction,
-# you’ll get little or no relative tax savings from buying. If your house-related
-# deductions are large enough to make itemizing worthwhile, we only count as
-# savings the amount above the standard deduction.
-
-# Opportunity Costs:
-# Opportunity costs are calculated for the initial purchase costs and for the
-# recurring costs. That will give you an idea of how much you could have made if
-# you had invested your money instead of buying your home.
-
-# Net Proceeds:
-# Net proceeeds is the amount of money you receive from the sale of your home
-# minus the closing costs, which includes the broker’s commission and other
-# fees, the remaining principal balance that you pay to your mortgage bank and
-# any tax you have to pay on profit that exceeds your capital gains exclusion.
-# If your total is negative, it means you have done very well: You made enough
-# of a profit that it covered not only the cost of your home, but also all of
-# your recurring expenses.
-"""
-
 class Home:
     def __init__(self):
         self.price = 2000000
